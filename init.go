ef3c998a123474c75d0f78ce6aee394558857cc5
Hank Shen <swh@admpub.com>
2021-12-15 13:52:10 +0800

    update

--- init.go
+++ init.go
@@ -1,9 +1,12 @@
 package caddymanager
 
 import (
+	"github.com/nging-plugins/caddymanager/pkg/handler"
 	_ "github.com/nging-plugins/caddymanager/pkg/library/cmder"
 	_ "github.com/nging-plugins/caddymanager/pkg/library/setup"
 )
+
+var LeftNavigate = handler.LeftNavigate
 
 func init() {
 
