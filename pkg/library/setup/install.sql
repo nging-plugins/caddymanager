620125514bfd08e6ced5056fab4d750adc9e7cda
Hank Shen <swh@admpub.com>
2021-12-15 11:40:21 +0800

    update

--- pkg/library/setup/install.sql
+++ pkg/library/setup/install.sql
@@ -98,4 +98,4 @@ CREATE TABLE `nging_vhost_group` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-14 13:32:53
+-- Dump completed on 2021-12-15 11:40:06
