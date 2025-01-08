872219c861a350a420f3882ead5112a40fee076d
Hank Shen <swh@admpub.com>
2025-01-08 18:51:18 +0800

    update

--- application/handler/navigate.go
+++ application/handler/navigate.go
@@ -1,132 +1,135 @@
 package handler
 
-import "github.com/coscms/webcore/library/navigate"
+import (
+	"github.com/coscms/webcore/library/navigate"
+	"github.com/webx-top/echo"
+)
 
 var LeftNavigate = &navigate.Item{
 	Display: true,
-	Name:    `网站管理`,
+	Name:    echo.T(`网站管理`),
 	Action:  `caddy`,
 	Icon:    `sitemap`,
 	Children: &navigate.List{
 		{
 			Display: false,
-			Name:    `Caddy日志`,
+			Name:    echo.T(`Caddy日志`),
 			Action:  `log_show`,
 		},
 		{
 			Display: true,
-			Name:    `网站列表`,
+			Name:    echo.T(`网站列表`),
 			Action:  `vhost`,
 		},
 		{
 			Display: true,
-			Name:    `添加网站`,
+			Name:    echo.T(`添加网站`),
 			Action:  `vhost_add`,
 			Icon:    `plus`,
 		},
 		{
 			Display: false,
-			Name:    `重启Caddy`,
+			Name:    echo.T(`重启Caddy`),
 			Action:  `restart`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `停止Caddy`,
+			Name:    echo.T(`停止Caddy`),
 			Action:  `stop`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `查看网站日志`,
+			Name:    echo.T(`查看网站日志`),
 			Action:  `vhost_log`,
 		},
 		{
 			Display: false,
-			Name:    `查看网站动态`,
+			Name:    echo.T(`查看网站动态`),
 			Action:  `log`,
 		},
 		{
 			Display: false,
-			Name:    `配置表单`,
+			Name:    echo.T(`配置表单`),
 			Action:  `addon_form`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `修改网站`,
+			Name:    echo.T(`修改网站`),
 			Action:  `vhost_edit`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `删除网站`,
+			Name:    echo.T(`删除网站`),
 			Action:  `vhost_delete`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `管理网站文件`,
+			Name:    echo.T(`管理网站文件`),
 			Action:  `vhost_file`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `生成Caddyfile`,
+			Name:    echo.T(`生成Caddyfile`),
 			Action:  `vhost_build`,
 			Icon:    ``,
 		},
 
 		{
 			Display: true,
-			Name:    `分组管理`,
+			Name:    echo.T(`分组管理`),
 			Action:  `group`,
 		},
 		{
 			Display: true,
-			Name:    `添加分组`,
+			Name:    echo.T(`添加分组`),
 			Action:  `group_add`,
 			Icon:    `plus`,
 		},
 		{
 			Display: false,
-			Name:    `修改分组`,
+			Name:    echo.T(`修改分组`),
 			Action:  `group_edit`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `删除分组`,
+			Name:    echo.T(`删除分组`),
 			Action:  `group_delete`,
 			Icon:    ``,
 		},
 
 		{
 			Display: true,
-			Name:    `引擎配置`,
+			Name:    echo.T(`引擎配置`),
 			Action:  `server`,
 		},
 		{
 			Display: false,
-			Name:    `添加引擎配置`,
+			Name:    echo.T(`添加引擎配置`),
 			Action:  `server_add`,
 			Icon:    `plus`,
 		},
 		{
 			Display: false,
-			Name:    `修改引擎配置`,
+			Name:    echo.T(`修改引擎配置`),
 			Action:  `server_edit`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `删除引擎配置`,
+			Name:    echo.T(`删除引擎配置`),
 			Action:  `server_delete`,
 			Icon:    ``,
 		},
 		{
 			Display: false,
-			Name:    `更新HTTPS证书`,
+			Name:    echo.T(`更新HTTPS证书`),
 			Action:  `server_renew_cert`,
 			Icon:    ``,
 		},
