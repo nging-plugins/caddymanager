ff519b541d779e02f85d98c7e5ab437cc0993ef5
Hank Shen <swh@admpub.com>
2025-01-18 16:55:52 +0800

    update

--- application/library/thirdparty/caddy2/init_dir.sh
+++ application/library/thirdparty/caddy2/init_dir.sh
@@ -1,4 +1,4 @@
-caddy2dir=/Users/hank/go/src/github.com/nging-plugins/caddymanager/example/data/test/caddy2
+caddy2dir=/Users/hank/go/src/github.com/nging-plugins/caddymanager/_example/data/test/caddy2
 mkdir -p ${caddy2dir}/vhosts/
 mkdir -p ${caddy2dir}/certs/
 mkdir -p ${caddy2dir}/html/
